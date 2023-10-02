2c4249c3bc2d05796b0b962f9ce0b9e64e5c9206
PRich57 <pcrichards57@gmail.com>
2023-10-02 17:29:39 -0600

    Create connection in connection.js

---
+++ config/connection.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+const Sequelize = require('sequelize');
+
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+      host: 'localhost',
+      dialect: 'mysql',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
+
+module.exports = sequelize;
