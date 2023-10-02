1ca757a7261be7e7aaf1b8470e9870d148faa348
PRich57 <pcrichards57@gmail.com>
2023-10-02 17:38:27 -0600

    Modify connection.js and  .gitignore to include .DS-STORE

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,16 @@
-require('dotenv').config();
+const Sequelize = require("sequelize");
 
-const Sequelize = require('sequelize');
+PORT = null || 3306;
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-      host: 'localhost',
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  provess.env.DB_PASS,
+  {
+    host: "localhost",
+    dialect: "mysql",
+    port: PORT,
+  }
+);
 
 module.exports = sequelize;
