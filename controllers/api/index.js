cb9f0fc62a7b11529772c8b4375572ce33064f58
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2023-10-03 15:55:49 -0600

    add user-routes.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,3 +1,6 @@
 const router = require("express").Router();
+const user = require("./user-routes");
+
+router.use("/user", user);
 
 module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,3 +1,5 @@
 const router = require("express").Router();
 
+router.get("/", (req, res) => {});
+
 module.exports = router;
