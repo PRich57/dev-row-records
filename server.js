2b31de4076280963179545207f98bcc79b4b6f01
PRich57 <pcrichards57@gmail.com>
2023-10-03 14:58:08 -0600

    Update server.js content

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
 
 const hbs = exphbs.create({});
 
