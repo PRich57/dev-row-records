5d79fde3b89a8f655af91f59cadbe3b4b707786e
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2023-10-03 18:36:53 -0600

    add skeleton routing to all existing handlebars templates

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,23 @@
 const router = require("express").Router();
 
-router.get("/", (req, res) => {});
+router.get("/", async (req, res) => {
+  // TODO: pull data from models and send to view.
+  res.status(200).render("homepage");
+});
+
+router.get("/artists", async (req, res) => {
+  // TODO: pull data from models and send to view.
+  res.status(200).render("artists");
+});
+
+router.get("/music", async (req, res) => {
+  // TODO: pull data from models and send to view.
+  res.status(200).render("albums");
+});
+
+router.get("/merch", async (req, res) => {
+  // TODO: pull data from models and send to view.
+  res.status(200).render("merch");
+});
 
 module.exports = router;
