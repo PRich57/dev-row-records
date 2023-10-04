cebe80d8ffe04b0378e05e0e5a98d9dcd45fc8ad
Coderinsightmaven <lmarincik@proton.me>
2023-10-04 11:05:57 -0600

    fixed the model errors

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const Category = require("./models/Category");
+const Merch = require("./models/Merch");
+const Artist = require("./models/Artist");
 
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
@@ -33,6 +36,20 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log("Now listening"));
-});
+Category.sync()
+  .then(() => {
+    // After Category is synced, sync the Artist model (assuming it also has no dependencies)
+    return Artist.sync();
+  })
+  .then(() => {
+    // After Artist is synced, sync the Merch model since it references both Category and Artist
+    return Merch.sync();
+  })
+  // Add additional models here if necessary, following the dependency order
+  .then(() => {
+    // Start the server only after all necessary models have been synced
+    app.listen(PORT, () => console.log("Now listening"));
+  })
+  .catch((error) => {
+    console.error("Error during model synchronization:", error);
+  });
