d56ab3133ac393b3df7b71079102d351e64e93ea
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2023-10-04 17:52:03 -0600

    write user login route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,40 @@
 const router = require("express").Router();
+const { User } = require("../../models");
 
-router.post("/login", (req, res) => {});
+// TODO: CREATE USER route
+router.post("/", async (req, res) => {});
+
+router.post("/login", async (req, res) => {
+  const { user_name, password } = req.body;
+  const user = await User.findOne({
+    where: {
+      user_name,
+    }
+  });
+  if (!user) {
+    res.status(400).json({
+      message: "Invalid credential. Try again."
+    });
+    return;
+  }
+  const validPassword = user.checkPassword(password);
+  if (!validPassword) {
+    res.status(400).json({
+      message: "Invalid credential. Try again."
+    });
+    return;
+  }
+  // TODO: set req.session.loggedIn to true, save userID to a cookie for use with the "favorites" tab.
+  req.session.save(() => {
+    req.session.loggedIn = true;
+    res.status(200).json({
+      message: "Successfully logged in!",
+      user,
+    });
+  })
+});
+
+// TODO: LOGOUT route
+router.post("/", async (req, res) => {});
 
 module.exports = router;
