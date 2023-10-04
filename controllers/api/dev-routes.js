ca2da44a76ab2c74c3b50c7ab03965422201b1e3
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2023-10-04 12:32:59 -0600

    add dev/album post route

--- controllers/api/dev-routes.js
+++ controllers/api/dev-routes.js
@@ -22,4 +22,40 @@ router.post("/artist", async (req, res) => {
   }
 });
 
+router.post("/album", async (req, res) => {
+  // get artist id from artist name
+  const artist_id = await findArtistIDByName(req, res);
+  console.log(artist_id);
+
+  // create album with artist id
+  const { name } = req.body;
+  try {
+    const data = await Album.create({ artist_id, name });
+    res.status(200).json({
+      message: "Successfully created album:",
+      data,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "Internal error while creating album",
+      err,
+    });
+  }
+});
+
+async function findArtistIDByName(req, res) {
+  const { artist_name } = req.body;
+  try {
+    const artist = await Artist.findOne({
+      where: {
+        name: artist_name,
+      },
+    });
+    console.log(artist);
+    return artist.id;
+  } catch (err) {
+    throw new Error(err);
+  }
+}
+
 module.exports = router;
