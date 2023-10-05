e88292c2a81983a13d928549ef42b6877d440105
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2023-10-05 13:30:58 -0600

    add modular routing structure for API routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,18 @@
 const router = require("express").Router();
 const user = require("./user-routes");
 const dev = require("./dev-routes");
+const album = require("./album-routes");
+const artist = require("./artist-routes");
+const genre = require("./genre-routes");
+const merch = require("./merch-routes");
+const tag = require("./tag-routes");
 
 router.use("/user", user);
 router.use("/dev", dev);
+router.use("/album", album);
+router.use("/artist", artist);
+router.use("/genre", genre);
+router.use("/merch", merch);
+router.use("/tag", tag);
 
 module.exports = router;
